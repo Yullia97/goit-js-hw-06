034bbd6f089549b0bce9960025f2886408ab0ee5
Yullia97 <146541652+Yullia97@users.noreply.github.com>
2023-11-29 10:12:54 +0200

    copy

--- js/task-02.js
+++ js/task-02.js
@@ -7,13 +7,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
- const fragment = document.createDocumentFragment();
 
-    ingredients.forEach(ingredient => {
+
+      const listItems = ingredients.map(ingredient => {
       const listItem = document.createElement('li');
       listItem.textContent = ingredient;
       listItem.classList.add('item');
-      fragment.appendChild(listItem);
+       return listItem;
     });
 
-    ingredientsList.appendChild(fragment);
+      ingredientsList.append(...listItems);
